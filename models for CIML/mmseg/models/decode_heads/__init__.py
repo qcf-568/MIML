6827d29f96d784c0db086904cf49249de7ccde56
qcf-568 <128270723+qcf-568@users.noreply.github.com>
2024-03-18 16:36:34 +0800

    Update __init__.py

--- models for CIML/mmseg/models/decode_heads/__init__.py
+++ models for CIML/mmseg/models/decode_heads/__init__.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .fcn_head import FCNHead
+from .uper_lab import UPerLab
 from .uper_head import UPerHead
 from .sep_aspp_head import DepthwiseSeparableASPPHead
 
 __all__ = [
-    'FCNHead', 'UPerHead', 'DepthwiseSeparableASPPHead'
+    'FCNHead', 'UPerLab', 'UPerHead', 'DepthwiseSeparableASPPHead'
 ]
