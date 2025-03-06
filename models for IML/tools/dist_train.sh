4f1801d1a4d5a7684d8d58233b7b0b1b00e0ce6a
qcf-568 <128270723+qcf-568@users.noreply.github.com>
2025-03-06 23:34:20 +0800

    Update dist_train.sh

--- models for IML/tools/dist_train.sh
+++ models for IML/tools/dist_train.sh
@@ -6,7 +6,7 @@ PORT=${PORT:-29666}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python -m torch.distributed.launch \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9  python -m torch.distributed.launch \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
