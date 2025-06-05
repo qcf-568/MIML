1f06f5880987687e7c32b0f6a515634d2a957cf7
qcf-568 <128270723+qcf-568@users.noreply.github.com>
2025-06-05 08:05:54 +0800

    Update __init__.py

--- models for IML/mmseg/models/decode_heads/__init__.py
+++ models for IML/mmseg/models/decode_heads/__init__.py
@@ -3,7 +3,7 @@ from .fcn_head import FCNHead
 from .psp_head import PSPHead
 from .sep_aspp_head2  import DepthwiseSeparableASPPHead2
 from .test_rfb1ha1c import TestRFB1HA1CLab
-
+from .decode_seg import Decode_seg 
 __all__ = [
-    'FCNHead', 'PSPHead', 'DepthwiseSeparableASPPHead2', 'TestRFB1HA1CLab'
+    'FCNHead', 'PSPHead', 'DepthwiseSeparableASPPHead2', 'TestRFB1HA1CLab', 'Decode_seg'
 ]
