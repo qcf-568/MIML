4bf01e22f521cdfb9a6281475db1546ae0e3892c
qcf-568 <128270723+qcf-568@users.noreply.github.com>
2025-08-30 17:54:49 +0800

    Update tianchi2022.py

--- models for IML/mmseg/datasets/tianchi2022.py
+++ models for IML/mmseg/datasets/tianchi2022.py
@@ -19,3 +19,33 @@ class TamperDatajpgpng(CustomDataset):
             seg_map_suffix='.png',
             **kwargs)
         assert osp.exists(self.img_dir),self.img_dir
+
+
+@DATASETS.register_module()
+class TamperCOCO(CustomDataset):
+    CLASSES = ('background', 'modify')
+    PALETTE = [[0, 0, 0], [255, 0, 0]]
+    def __init__(self, **kwargs):
+        super(TamperCOCO, self).__init__(
+            img_suffix='.jpg',
+            seg_map_suffix='.png',
+            **kwargs)
+        assert osp.exists(self.img_dir),self.img_dir
+        with open('coco.pk', 'rb') as f:
+            self.img_infos = pickle.load(f)
+
+    def prepare_train_img(self, idx):
+        img_info = self.img_infos[idx]
+        results = dict(img_info=dict(filename=os.path.join(self.img_dir, img_info['p'])), ann_info=None, gt_bboxes=img_info['b'], mask=img_info['m'])
+        self.pre_pipeline(results)
+        return self.pipeline(results)
+
+    def __getitem__(self, idx):
+      try:
+        if self.test_mode:
+            return self.prepare_test_img(idx)
+        else:
+            return self.prepare_train_img(idx)
+      except:
+        print('error')
+        return self.__getitem__(random.randint(0,self.__len__()))
